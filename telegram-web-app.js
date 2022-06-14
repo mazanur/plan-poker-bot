7d6c0a4d95a81549392a3f4118b37b51ac47d59b
Almaz Nurmukhametov <almazic91@gmail.com>
2022-06-14 12:50:42 +0300

    Update telegram-web-app.js

--- telegram-web-app.js
+++ telegram-web-app.js
@@ -311,7 +311,10 @@
   if (theme_params) {
     setThemeParams(theme_params);
   }
-
+  if (initParams.tgWebAppVersion) {
+    webAppVersion = initParams.tgWebAppVersion;
+  }
+  
 
   function onThemeChanged(eventType, eventData) {
     if (eventData.theme_params) {
